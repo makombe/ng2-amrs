c7940830ceb9e2915b8a28196ad7d844f93f8bb7
makombe <kennedymakombe@gmail.com>
2018-04-17 09:42:13 +0300

    All forms

--- src/app/patient-dashboard/common/forms/form-list.service.ts
+++ src/app/patient-dashboard/common/forms/form-list.service.ts
@@ -64,9 +64,10 @@ export class FormListService {
 
         _.each(unsortArray,
             (item) => {
-                if (item.published === true) {
+               // if (item.published === true) {
+              
                     publishedOpenmrsForms.push(item);
-                }
+               // }
             });
         return publishedOpenmrsForms;
     }
